913ffe984ab8df6155cdb7d8f2f88d6113e97319
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-11-27 21:08:17 +0100

    Add gift list getter to Santa-Class

--- include/Santa.hpp
+++ include/Santa.hpp
@@ -13,13 +13,13 @@ class Santa
         int load(const std::vector<Gift>& giftList);
         void start_delivering(void);
 
-        double getSleighMaxLoad(void) const { return sleigh_max_load; }
-        double getSleighWeight(void) const { return sleigh_weight; }
-
-        double getSleighCurrentLoad(void) const { return sleigh_current_load; }
-        Coordinate getCurrentPos(void) const { return current_pos; }
-        double getWRW(void) const { return WRW; }
-        unsigned int getGiftToDeliver(void) const { return gifts_to_deliver; }
+        double getSleighMaxLoad(void) const { return this->sleigh_max_load; }
+        double getSleighWeight(void) const { return this->sleigh_weight; }
+        double getSleighCurrentLoad(void) const { return this->sleigh_current_load; }
+        Coordinate getCurrentPos(void) const { return this->current_pos; }
+        double getWRW(void) const { return this->WRW; }
+        unsigned int getGiftToDeliver(void) const { return this->gifts_to_deliver; }
+        std::vector<Gift>& getLoadedGifts(void) { return this->loaded_gifts; }
     
     private:
         const double sleigh_max_load = 1000.0;
