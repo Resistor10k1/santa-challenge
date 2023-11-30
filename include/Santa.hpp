94ac64673e26b511ffd1c78ee9b69e99dd80543b
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-11-30 07:30:49 +0100

    Cleanup and restructure Santa-Class

--- include/Santa.hpp
+++ include/Santa.hpp
@@ -5,21 +5,35 @@
 #include <vector>
 
 
+/**
+ * @brief Class abstracting santa.
+*/
 class Santa
 {
     public:
-        Santa(void);
-        int load(const Gift& gift);
+        Santa(double max_load);
+        int add(const Gift& gift);
         int load(const std::vector<Gift>& giftList);
-        void start_delivering(void);
+
+        /**
+         * @brief 'Delivers' the gifts by calculating the WRW.
+        */
+        double calculateWRW(void);
 
         double getSleighMaxLoad(void) const { return this->sleigh_max_load; }
         double getSleighWeight(void) const { return this->sleigh_weight; }
         double getSleighCurrentLoad(void) const { return this->sleigh_current_load; }
         Coordinate getCurrentPos(void) const { return this->current_pos; }
         double getWRW(void) const { return this->WRW; }
-        unsigned int getGiftToDeliver(void) const { return this->gifts_to_deliver; }
-        std::vector<Gift>& getLoadedGifts(void) { return this->loaded_gifts; }
+        // unsigned int getGiftsToDeliver(void) const { return this->gifts_to_deliver; }
+
+        std::vector<Gift> getLoadedGifts(void) { return this->loaded_gifts; }
+        void setLoadedGifts(std::vector<Gift> giftList) { this->loaded_gifts = giftList; }
+
+        void unloadSleigh(void)
+        {
+            this->reset_attributes();
+        }
     
     private:
         const double sleigh_max_load = 1000.0;
@@ -28,5 +42,7 @@ class Santa
         double WRW = 0.0;
         Coordinate current_pos;
         std::vector<Gift> loaded_gifts;
-        unsigned int gifts_to_deliver = 0;
+        // unsigned int gifts_to_deliver = 0;
+
+        void reset_attributes(void);
 };
--- source/Santa.cpp
+++ source/Santa.cpp
@@ -2,59 +2,71 @@
 #include "Santa.hpp"
 #include "misc.hpp"
 
-Santa::Santa(void) : current_pos({90.0, 0.0})
+Santa::Santa(double max_load) : sleigh_max_load(max_load), current_pos({90.0, 0.0})
 {
 
 }
 
-int Santa::load(const Gift& gift)
+void Santa::reset_attributes(void)
+{
+    this->sleigh_current_load = 0.0;
+    this->WRW = 0.0;
+    this->current_pos = {90.0, 0.0};
+    this->loaded_gifts.clear();
+}
+
+int Santa::add(const Gift& gift)
 {
     // Check if a delivery is not running
-    if(gifts_to_deliver == loaded_gifts.size())
-    {
+    // if(gifts_to_deliver == loaded_gifts.size())
+    // {
         if((sleigh_current_load+gift.weight()) > sleigh_max_load)
             return -1; //throw std::runtime_error("Sleigh max load exceeded!");
         
         sleigh_current_load += gift.weight();
         loaded_gifts.push_back(gift);
-        ++gifts_to_deliver;
-    }
-    else
-    {
-        return -2;
-    }
+        // ++gifts_to_deliver;
+    // }
+    // else
+    // {
+    //     return -2;
+    // }
     return 0;
 }
 
 int Santa::load(const std::vector<Gift>& giftList)
 {
     int ret_val = 0;
+    this->reset_attributes();
+
     for(auto& gift : giftList)
     {
-        ret_val = load(gift);
+        ret_val = add(gift);
         if(ret_val != 0) break;
     }
     return ret_val;
 }
 
-void Santa::start_delivering(void)
+double Santa::calculateWRW(void)
 {
     this->WRW = 0.0;
     
     auto gift = loaded_gifts.begin();
     for(; gift != loaded_gifts.end(); ++gift)
     {
-        WRW += (sleigh_weight + sleigh_current_load) * haversine(current_pos, gift->getCoordinate(), degree);
-        sleigh_current_load -= gift->weight();
-        current_pos = gift->getCoordinate();
-        --gifts_to_deliver;
+        this->WRW += (sleigh_weight + sleigh_current_load) * haversine(current_pos, gift->getCoordinate(), degree);
+        this->sleigh_current_load -= gift->weight();
+        this->current_pos = gift->getCoordinate();
+        // --gifts_to_deliver;
     }
     WRW += sleigh_weight * haversine(current_pos, {90.0, 0.0}, degree);
+    this->current_pos = {90.0, 0.0};
 
     // Reset sleigh
-    loaded_gifts.clear();
-    sleigh_current_load = 0.0;
-    gifts_to_deliver = 0;
-    current_pos = {90.0, 0.0};
+    // loaded_gifts.clear();
+    // sleigh_current_load = 0.0;
+    // gifts_to_deliver = 0;
+
+    return this->WRW;
 }
 
