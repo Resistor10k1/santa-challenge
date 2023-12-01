bde1c5ca7fc87cb32d469b8b829b03e8a4391d78
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-12-01 19:24:20 +0100

    Implement Nearest-Neighbour loading

--- source/LoadStrategy.cpp
+++ source/LoadStrategy.cpp
@@ -44,29 +44,44 @@ std::vector<Gift>::iterator NearestLoadingStrategy::loadTourToSleigh(Santa& sant
 void NNLoadingStrategy::preprocessGifts(std::vector<Gift>& giftList)
 {
     sort_distance(giftList);
-
-    for(auto gift : giftList)
-    {
-        this->dist_matrix.push_back(std::vector<Gift>(giftList.size()));
-
-        for(auto other : giftList)
-        {
-            
-        }
-    }
 }
 
 std::vector<Gift>::iterator NNLoadingStrategy::loadTourToSleigh(Santa& santa, const std::vector<Gift>::iterator& it_start,
                                                                 const std::vector<Gift>::iterator& it_end,
                                                                 unsigned int tour_nbr)
 {
-    auto gift = it_start;
+    auto ref_gift = it_start;
+
     santa.unloadSleigh();
 
-    while((loadSleigh(santa, *gift, tour_nbr)==0) && (gift!=it_end))
+    for(auto neighbour=it_end, gift_it=it_start; ref_gift!=it_end; neighbour=it_end, gift_it=it_start)
     {
-        ++gift;
+        double current_nearest = __DBL_MAX__;
+
+        while(gift_it != it_end)
+        {/* Find the nearest neighbour */
+            if(((*gift_it).getTourNumber() == 0) && (gift_it != ref_gift))
+            {/* Gift has not been loaded yet */
+                double dist_neighbour = haversine((*ref_gift).getCoordinate(), (*gift_it).getCoordinate(), degree);
+                if(current_nearest > dist_neighbour)
+                {
+                    neighbour = gift_it;
+                    current_nearest = dist_neighbour;
+                }
+            }
+            ++gift_it;
+        }
+
+        if(neighbour == it_end)
+        {/* No gifts left */
+            return it_end;
+        }
+        else
+        {
+            ref_gift = neighbour;
+            if(loadSleigh(santa, *neighbour, tour_nbr) != 0) return ref_gift;
+        }
     }
 
-    return gift;
+    return ref_gift;
 }
