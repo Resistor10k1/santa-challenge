d51dd996440cc53d9d08ea0cf4007fd65216b998
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-12-08 18:08:16 +0100

    Improve Simulated Annealing

--- source/DistributeStrategy.cpp
+++ source/DistributeStrategy.cpp
@@ -19,31 +19,34 @@ void SimulatedAnnealingStrategy::distributeGifts(Santa& santa)
 {
     double temp_WRW = __DBL_MAX__;
     double local_best;
+    Santa local_santa = santa;
     std::vector<Gift> s;
     #pragma omp critical
     {
-        local_best = santa.calculateWRW();
-        s = santa.getLoadedGifts();
+        local_best = local_santa.calculateWRW();
+        this->best_WRW = local_best;
+        // std::cout << "Start value " << omp_get_thread_num() << ": " << local_best << std::endl;
+        s = local_santa.getLoadedGifts();
     }
-
+    #pragma omp barrier
     auto s_star = s;
-    double T = 10000;
-    double T_final = 20;
-    const double alpha = 0.99;
+    double T = 100000;
+    double T_final = 0.001;
+    const double alpha = 0.9;
     unsigned int loop_cnt = 0;
     int nbr_improvements = 0;
     int nbr_reheats = 0;
 
     // TODO: Chose a different termination condition
     // for(unsigned int loop_cnt = 0; (T > T_final) && ((nbr_reheats-nbr_improvements) > 5); ++loop_cnt)
-    while((T > T_final) && ((nbr_reheats-nbr_improvements) < 5))
+    while((T > T_final) /*&& ((nbr_reheats-nbr_improvements) < 15)*/)
     {
         applyRandomSwap(s);
 
         #pragma omp critical
         {
-            santa.load(s);
-            temp_WRW = santa.calculateWRW();
+            local_santa.load(s);
+            temp_WRW = local_santa.calculateWRW();
         }
 
         double D = temp_WRW - local_best;
@@ -51,35 +54,43 @@ void SimulatedAnnealingStrategy::distributeGifts(Santa& santa)
         if((D < 0) || (std::exp(-D/T) > rndU()))
         {
             #pragma omp critical
-            s = santa.getLoadedGifts();
+            s = local_santa.getLoadedGifts();
         }
 
         if(temp_WRW < local_best)
         {
             s_star = s;
             local_best = temp_WRW;
+            ++nbr_improvements;
         }
 
-        if(loop_cnt == 200)
+        if((loop_cnt+1)%200 == 0)
         {
-            T *= 1.8;
+            T *= 1.3;
             ++nbr_reheats;
         }
-        else
+        else if((loop_cnt+1)%12 == 0)
         {
             T *= alpha;
         }
+
+        if((loop_cnt-nbr_improvements) > 100)
+        {
+            s = s_star;
+        }
+
         ++loop_cnt;
     }
     
     #pragma omp critical
     {
-        santa.load(s_star);
-        this->best_WRW = local_best;
+        if(local_best < this->best_WRW)
+        {
+            std::cout <<"Thread-ID "<<omp_get_thread_num()<<": "<<T<<", "<<T_final<<", "<<nbr_reheats<<", "<<nbr_improvements<<", "<<loop_cnt<<", "<<this->best_WRW<<", "<<local_best<<std::endl;
+            santa.load(s_star);
+            this->best_WRW = local_best;
+        }
     }
-
-    // #pragma omp critical
-    // std::cout <<"Thread-ID "<<omp_get_thread_num()<<": "<<T<<", "<<T_final<<", "<<nbr_reheats<<", "<<nbr_improvements<<", "<<temp_WRW<<std::endl;
 }
 
 double SimulatedAnnealingStrategy::rndU(void)
@@ -95,10 +106,16 @@ void SimulatedAnnealingStrategy::applyRandomSwap(std::vector<Gift>& giftList)
 {
     std::random_device rd;
     std::mt19937_64 gen(rd());
-    std::uniform_int_distribution<> dist(0, giftList.size()-1);
+    std::uniform_int_distribution<> init_dist(0, giftList.size()-1);
+    std::uniform_int_distribution<> offset_dist(2, 5);
 
-    int id_1 = dist(gen);
-    int id_2 = dist(gen);
+    int start_id = init_dist(gen);
+    int offset_id = offset_dist(gen);
 
-    std::iter_swap(giftList.begin()+id_1, giftList.begin()+id_2);
+    if((start_id+offset_id) >= giftList.size())
+    {
+        offset_id *= -1;
+    }
+
+    std::iter_swap(giftList.begin()+start_id, giftList.begin()+start_id+offset_id);
 }
